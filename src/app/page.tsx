66b0daced0f9d398bf475de2873df6bd721b2fc3
Yujie <yp25@st-andrews.ac.uk>
2024-07-01 11:13:09 +0100

    test convex query function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { useMutation } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 import { api } from "../../convex/_generated/api"
 
 export default function Home() {
   const createFile = useMutation(api.files.createFile)
+  const files = useQuery(api.files.getFiles)
   return (
-    <main className="flex min-h-screen flex-col justify-between">
+    <main className="flex flex-col justify-between">
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>
+      })}
       <Button
         onClick={() => {
           createFile({ name: "test" })
