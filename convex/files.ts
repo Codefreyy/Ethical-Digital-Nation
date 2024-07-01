8ec44b1f5a7faf1255ececa53aa9af742e5d6ad8
Yujie <yp25@st-andrews.ac.uk>
2024-07-01 11:18:02 +0100

    add authentication for create and get file functions

--- convex/files.ts
+++ convex/files.ts
@@ -6,6 +6,10 @@ export const createFile = mutation({
         name: v.string()
     },
     async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity()
+        if (!identity) {
+            throw new Error('not authenticated')
+        }
         await ctx.db.insert('files', {
             name: args.name
         })
@@ -15,6 +19,10 @@ export const createFile = mutation({
 export const getFiles = query({
     args: {},
     async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity()
+        if (!identity) {
+            return []
+        }
         return ctx.db.query('files').collect()
     }
 })
